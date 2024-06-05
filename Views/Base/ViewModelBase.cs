f9237f197281196847b0b3c8efd0a49931aa6adf
chrisrekfild <chrisrekfild@mail.ru>
2024-06-05 23:53:21 +0500

    Добавьте файлы проекта.

---
+++ Views/Base/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.Views.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnProperyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Устанавливает значение поля, при этом разрешая проблему кольцевых изменений
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field">Поле, в которое происходит запись</param>
+        /// <param name="value">Новое значение, которое будет записано в поле (при успехе, см. ниже)</param>
+        /// <param name="propertyName">имя свойства. Можно не указывать. P.S. да, я ленив =)</param>
+        /// <returns>True, если значение успешно изменено. False, если данные в поле уже совпадают с тем, что мы хотим туда записать</returns>
+        protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnProperyChanged(propertyName);
+
+            return true;
+        }
+    }
+}
---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Unicode;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textbox_path.Text = AppDomain.CurrentDomain.BaseDirectory;
+
+            string appDir = AppDomain.CurrentDomain.BaseDirectory;
+            string xmlDowloadDir = $"{appDir}\\Downloads";
+            if (!Directory.Exists(xmlDowloadDir))
+                Directory.CreateDirectory(xmlDowloadDir);
+
+            string url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=25/05/2023";
+
+            using (var client = new HttpClient())
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                client
+                    .DefaultRequestHeaders
+                    .Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Xml));
+                client.Timeout = TimeSpan.FromSeconds(500);
+                var response = await client.GetAsync(url).ConfigureAwait(false);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContentBytes = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+                    var enc1251 = CodePagesEncodingProvider.Instance.GetEncoding("windows-1251");
+                    string responseContent = enc1251.GetString(responseContentBytes);
+
+                    if (string.IsNullOrWhiteSpace(responseContent) == false)
+                    {
+                        using (StreamWriter writer = new StreamWriter($"{xmlDowloadDir}\\25_05_2023.xml", false, enc1251))
+                        {
+                            writer.Write(responseContent);
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+}

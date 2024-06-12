9b8ada1f43d55bda080fe528912112a08f90aaec
chrisrekfild <chrisrekfild@mail.ru>
2024-06-12 19:26:46 +0500

    Убрал лишние ссылки из MainWindow

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -7,22 +7,10 @@ using System.Net.Mime;
 using System.Security.Policy;
 using System.Text;
 using System.Text.Unicode;
-using System.Web;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CurrencyConverter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
